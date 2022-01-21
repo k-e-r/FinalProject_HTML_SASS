9f4208196addb5ca0ae679f931f64bb03349d41d
k-e-r <damajonboua@gmail.com>
2022-01-20 17:12:57 -0800

    modify search function

--- js/header.js
+++ js/header.js
@@ -1,15 +1,15 @@
-const modal = document.querySelector(".menu-mobile-container");
-const modalButton = document.querySelector(".menu-toggle");
+const modal = document.querySelector('.menu-mobile-container');
+const modalButton = document.querySelector('.menu-toggle');
 
-const searchBar = document.querySelector(".search-container");
-const searchButton = document.querySelector(".site-search");
+const searchBar = document.querySelector('.search-container');
+const searchButton = document.querySelector('.site-search');
 
 modalButton.onclick = () => {
-  modal.style.display === "none"
-    ? (modal.style.display = "block")
-    : (modal.style.display = "none");
+  modal.style.display === 'none'
+    ? (modal.style.display = 'block')
+    : (modal.style.display = 'none');
 };
 
 searchButton.onclick = () => {
-  searchBar.classList.toggle("active");
+  searchBar.classList.toggle('active');
 };
